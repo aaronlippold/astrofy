cec46f767c619bbdd0d1bb8983329a0d6fb36db9
Aaron Lippold <lippold@gmail.com>
2024-10-06 23:31:44 -0400

    update site url to netlify, added netlify.toml
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://carmelina-ayala.netlify.app',
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'server',
+  adapter: netlify()
 });
